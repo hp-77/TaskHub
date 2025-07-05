894dc8afd5d4604cef78d110f5a91e5f72d6d3b2
hp-77 <harsh.pant.77@gmail.com>
2025-07-05 09:38:33 +0530

    fix: update CORS configuration to allow Vercel origin

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL,
+      `${process.env.FRONTEND_URL}/`,
+    ],
     methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
